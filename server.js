b20b82524870e7c0e4e01322cb66fe8f9438bb50
azishexacrew <azishexacrew@gmail.com>
2018-02-05 14:55:01 +0800

    beta version

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app); 
+
+app.use(express.static(__dirname + '/public')); 
+//redirect / to our index.html file
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + '/public/*');
+});
+
+//start our web server and socket.io server listening
+server.listen(4000, function(){
+  console.log('listening on *:4000');
+}); 
